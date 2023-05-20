d4cd87c1195755e970e567c7645502ef10bb0bf9
kerBamf <zach.pedigo@gmail.com>
2023-05-20 19:51:44 -0400

    Adding Auth, will need to move active user state to separate component to tidy things up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
+import { setUserToken, clearUserToken } from './utils/authToken'
 import TaskList from './pages/taskList'
 import ShowTask from './pages/showTask'
 import NewTask from './pages/newTask'
@@ -15,14 +16,72 @@ import Header from './components/header'
 import Container from 'react-bootstrap/Container'
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({})
+  const [isAuthenticated, setIsAuthenticated] = useState({})
   const [score, setScore] = useState(0)
   const [tasksDone, setTasksDone] = useState(0)
+
+  const registerUser = async (data) => {
+    try {
+      const configs = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+
+      const newUser = awaitFetch('http://localhost:400/auth/register',
+      configs)
+
+      const parsedUser = await newUser.json()
+      console.log(parsedUser)
+
+      setUserToken(parsedUser.token)
+      setCurrentUser(parsedUser.currentUser)
+      setIsAuthenticated(parsedUser.loggedIn)
+      return parsedUser
+    } catch(err) {
+      console.log(err)
+      clearUserToken();
+      setIsAuthenticated(false)
+  }
+}
+
+  const loginUser = async (data) => {
+    try {
+      const configs = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "applications/json"
+        }
+      }
+
+      const response = await fetch(
+        "http://localhost:4000/auth/login", configs
+      )
+
+      const user = await response.json()
+      console.log(user)
+
+      setUserToken(user.token)
+      setCurrentUser(user.currentUser)
+      setIsAuthenticated(user.loggedI)
+
+      return user
+    } catch(err) {
+      console.log(err)
+      clearUserToken()
+      setIsAuthenticated(false)
+    }
+  }
 
   return (
     <div className="App">
       <div className="appContainer">
       <Container fluid="xs">
-        <Header mods={{score, tasksDone}}/>
+        <Header mods={{score, tasksDone, currentUser}}/>
       <Routes>
         <Route path="/" element={<TaskList mods={{score, setScore, tasksDone, setTasksDone}} />} />
         <Route path="/task">
