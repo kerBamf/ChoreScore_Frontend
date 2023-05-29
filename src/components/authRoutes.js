e6cf12389d456bd2b846f19970621aae7a5c50ed
kerBamf <zach.pedigo@gmail.com>
2023-05-29 09:03:31 -0400

    Fixed more URLS

--- src/components/authRoutes.js
+++ src/components/authRoutes.js
@@ -29,7 +29,7 @@ export const registerUser = async (data) => {
 
  export const loginUser = async (data) => {
   //const URL = "http://localhost:4000"
-  const URL = "https://joyern.onrender.com/"
+  const URL = "https://joyern.onrender.com"
     try {
       const configs = {
         method: "POST",
@@ -54,7 +54,7 @@ export const registerUser = async (data) => {
 
   export const logoutUser = async (data) => {
     //const URL = "http://localhost:4000"
-    const URL = "https://joyern.onrender.com/"
+    const URL = "https://joyern.onrender.com"
     try{
         let response = await fetch(URL + '/auth/logout')
         response = response.json()
